9df76ef0e354420fd1124c5c8f80ace665efd59d
Zihao Xia <zxia10@uci.edu>
2025-04-12 22:19:26 -0700

    Commit Everything

---
+++ Maidle-react/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Maidle</h1>
+        <button>I don't know </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
